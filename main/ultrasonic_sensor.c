0042a33a548652bcd98394332334c20baa336036
fussionko <sebi.vidergar@gmail.com>
2023-07-18 19:26:56 +0200

    work on measure_cm function

--- main/ultrasonic_sensor.c
+++ main/ultrasonic_sensor.c
@@ -13,8 +13,11 @@
 #define ROUNDTRIP 58 // 20deg
 
 
+
 static portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 
+
+#define RETURN_CRITICAL(MUX, RES) do { portEXIT_CRITICAL(&MUX); return RES; } while (0)
 static inline uint32_t get_time_us()
 {
     struct timeval tv;
@@ -31,4 +34,23 @@ void ultrasonic_sensor_init(const ultrasonic_sensor_t* sensor)
     gpio_set_direction(sensor->echo, GPIO_MODE_INPUT);
 
     gpio_set_level(sensor->trigger, 0);
+}
+
+esp_err_t ultrasonic_sensor_measure_cm(const ultrasonic_sensor_t* sensor, uint32_t max_distance, uint32_t* distance)
+{
+    if (distance == NULL)
+        return ESP_ERR_INVALID_ARG;
+
+    portENTER_CRITICAL(&mux);
+
+    // Sets of ultrasonic sensor 
+    gpio_set_level(sensor->trigger, 0);
+    esp_rom_delay_us(TRIGGER_LOW_DELAY);
+    gpio_set_level(sensor->trigger, 1);
+    esp_rom_delay_us(TRIGGER_HIGH_DELAY);
+    gpio_set_level(sensor->trigger, 0);
+
+    // If previous ping hasn't ended
+    if (gpio_get_level(sensor->echo))
+        RETURN_CRITICAL(mux, ESP_ERR_ULTRASONIC_SENSOR_PING);
 }
