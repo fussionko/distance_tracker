15df1992432832e387824483ecf67bda391bdad9
fussionko <sebi.vidergar@gmail.com>
2023-07-18 23:02:22 +0200

    tasks & struct param

--- main/ultrasonic_sensor.c
+++ main/ultrasonic_sensor.c
@@ -12,12 +12,18 @@
 // TODO change with temp sensor -> humidity
 #define ROUNDTRIP 58 // 20deg
 
-
+typedef struct 
+{
+    esp_err_t error;
+    ultrasonic_sensor_t* ultrasonic_sensor;
+    uint32_t distance;
+} measure_param;
 
 static portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 
-
+#define timeout_expired(start, len) ((uint32_t)get_time_us() - start) >= len
 #define RETURN_CRITICAL(MUX, RES) do { portEXIT_CRITICAL(&MUX); return RES; } while (0)
+
 static inline uint32_t get_time_us()
 {
     struct timeval tv;
@@ -29,18 +35,17 @@ static inline uint32_t get_time_us()
 void ultrasonic_sensor_init(const ultrasonic_sensor_t* sensor)
 {
     gpio_reset_pin(sensor->trigger);
-    gpio_reset_pin(sensor->echo);
+    gpio_reset_pin(sensor->echo_left);
+    gpio_reset_pin(sensor->echo_right);
     gpio_set_direction(sensor->trigger, GPIO_MODE_OUTPUT);
-    gpio_set_direction(sensor->echo, GPIO_MODE_INPUT);
+    gpio_set_direction(sensor->echo_left, GPIO_MODE_INPUT);
+    gpio_set_direction(sensor->echo_right, GPIO_MODE_INPUT);
 
     gpio_set_level(sensor->trigger, 0);
 }
 
-esp_err_t ultrasonic_sensor_measure_cm(const ultrasonic_sensor_t* sensor, uint32_t max_distance, uint32_t* distance)
+esp_err_t ultrasonic_measure_left_cm(const ultrasonic_sensor_t* sensor, uint32_t* distance_left)
 {
-    if (distance == NULL)
-        return ESP_ERR_INVALID_ARG;
-
     portENTER_CRITICAL(&mux);
 
     // Sets of ultrasonic sensor 
@@ -51,6 +56,50 @@ esp_err_t ultrasonic_sensor_measure_cm(const ultrasonic_sensor_t* sensor, uint32
     gpio_set_level(sensor->trigger, 0);
 
     // If previous ping hasn't ended
-    if (gpio_get_level(sensor->echo))
+    if (gpio_get_level(sensor->echo_left))
         RETURN_CRITICAL(mux, ESP_ERR_ULTRASONIC_SENSOR_PING);
+
+    	
+    // Wait for echo reply from single reciever
+    uint32_t start = get_time_us();
+    while (!gpio_get_level(sensor->echo_left))
+    {
+        if (timeout_expired(start, PING_TIMEOUT))
+            RETURN_CRITICAL(mux, ESP_ERR_ULTRASONIC_SENSOR_PING_TIMEOUT);
+    }
+
+    // Echo recieved continue
+    uint32_t echo_start = get_time_us();
+    uint32_t time = echo_start;
+    uint32_t meas_timeout = echo_start + MAX_DISTANCE + ROUNDTRIP;
+    while (gpio_get_level(sensor->echo_left))
+    {
+        time = get_time_us();
+        if (timeout_expired(start, meas_timeout))
+            RETURN_CRITICAL(mux, ESP_ERR_ULTRASONIC_SENSOR_ECHO_TIMEOUT);
+    }
+
+    portEXIT_CRITICAL(&mux);
+
+    *distance_left = (time - echo_start) / ROUNDTRIP;
+
+    return ESP_OK;
+}
+
+esp_err_t ultrasonic_measure_right_cm(const ultrasonic_sensor_t* sensor, uint32_t* distance_right)
+{
+
+}
+
+
+esp_err_t ultrasonic_measure_cm(const ultrasonic_sensor_t* sensor, uint32_t* distance_left, uint32_t* distance_right)
+{
+    if (distance_left == NULL || distance_left == distance_right || sensor == NULL)
+        return ESP_ERR_INVALID_ARG;
+
+    TaskHandle_t measure_left = NULL;
+    TaskHandle_t measure_right = NULL;
+
+    xTaskCreate(ultrasonic_measure_left_cm, "Measure left sensor", 4096, NULL, 10, &measure_left);
+    xTaskCreatePinnedToCore(ultrasonic_measure_right_cm, "Measure lright sensor", 4096, NULL, 10, &measure_right, 1);
 }
--- main/ultrasonic_sensor.h
+++ main/ultrasonic_sensor.h
@@ -5,18 +5,23 @@
 #define ESP_ERR_ULTRASONIC_SENSOR_PING_TIMEOUT  0x201
 #define ESP_ERR_ULTRASONIC_SENSOR_ECHO_TIMEOUT  0x202
 
+#define MAX_DISTANCE
+
 #include <driver/gpio.h>
 
+// 1 trigger 2 echo pins
 typedef struct 
 {
     gpio_num_t trigger;
-    gpio_num_t echo;
+    gpio_num_t echo_left, echo_right;
 } ultrasonic_sensor_t;
 
 
 void ultrasonic_sensor_init(const ultrasonic_sensor_t* sensor);
 
 
-esp_err_t ultrasonic_sensor_measure_cm(const ultrasonic_sensor_t* sensor, uint32_t max_distance, uint32_t* distance);
+//esp_err_t ultrasonic_sensor_measure_cm(const ultrasonic_sensor_t* sensor, uint32_t* distance);
+
+esp_err_t ultrasonic_measure_cm(const ultrasonic_sensor_t* sensor, uint32_t* distance_left, uint32_t* distance_right);
 
 #endif /* _ULTRASONIC_SENSOR_H__ */
