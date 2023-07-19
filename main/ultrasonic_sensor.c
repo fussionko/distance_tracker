9ba9264b3e74d00ba4a08a1c749dfc6fee47c2d7
fussionko <sebi.vidergar@gmail.com>
2023-07-19 09:51:43 +0200

    ultrasonic_measure test

--- main/ultrasonic_sensor.c
+++ main/ultrasonic_sensor.c
@@ -22,7 +22,7 @@ typedef struct
 static portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 
 #define timeout_expired(start, len) ((uint32_t)get_time_us() - start) >= len
-#define RETURN_CRITICAL(MUX, RES) do { portEXIT_CRITICAL(&MUX); return RES; } while (0)
+#define RETURN_CRITICAL(MUX, RES, STORE) do { portEXIT_CRITICAL(&MUX); STORE->error = RES; vTaskDelete(NULL); } while (0)
 
 static inline uint32_t get_time_us()
 {
@@ -44,51 +44,96 @@ void ultrasonic_sensor_init(const ultrasonic_sensor_t* sensor)
     gpio_set_level(sensor->trigger, 0);
 }
 
-esp_err_t ultrasonic_measure_left_cm(const ultrasonic_sensor_t* sensor, uint32_t* distance_left)
+void ultrasonic_measure_left_cm(void* parameter)
 {
+    measure_param* param = (measure_param*)parameter;
+
     portENTER_CRITICAL(&mux);
 
     // Sets of ultrasonic sensor 
-    gpio_set_level(sensor->trigger, 0);
+    gpio_set_level(param->ultrasonic_sensor->trigger, 0);
     esp_rom_delay_us(TRIGGER_LOW_DELAY);
-    gpio_set_level(sensor->trigger, 1);
+    gpio_set_level(param->ultrasonic_sensor->trigger, 1);
     esp_rom_delay_us(TRIGGER_HIGH_DELAY);
-    gpio_set_level(sensor->trigger, 0);
+    gpio_set_level(param->ultrasonic_sensor->trigger, 0);
 
     // If previous ping hasn't ended
-    if (gpio_get_level(sensor->echo_left))
-        RETURN_CRITICAL(mux, ESP_ERR_ULTRASONIC_SENSOR_PING);
+    if (gpio_get_level(param->ultrasonic_sensor->echo_left))
+        RETURN_CRITICAL(mux, ESP_ERR_ULTRASONIC_SENSOR_PING, param);
 
     	
     // Wait for echo reply from single reciever
     uint32_t start = get_time_us();
-    while (!gpio_get_level(sensor->echo_left))
+    while (!gpio_get_level(param->ultrasonic_sensor->echo_left))
     {
         if (timeout_expired(start, PING_TIMEOUT))
-            RETURN_CRITICAL(mux, ESP_ERR_ULTRASONIC_SENSOR_PING_TIMEOUT);
+            RETURN_CRITICAL(mux, ESP_ERR_ULTRASONIC_SENSOR_PING_TIMEOUT, param);
     }
 
     // Echo recieved continue
     uint32_t echo_start = get_time_us();
     uint32_t time = echo_start;
     uint32_t meas_timeout = echo_start + MAX_DISTANCE + ROUNDTRIP;
-    while (gpio_get_level(sensor->echo_left))
+    while (gpio_get_level(param->ultrasonic_sensor->echo_left))
     {
         time = get_time_us();
         if (timeout_expired(start, meas_timeout))
-            RETURN_CRITICAL(mux, ESP_ERR_ULTRASONIC_SENSOR_ECHO_TIMEOUT);
+            RETURN_CRITICAL(mux, ESP_ERR_ULTRASONIC_SENSOR_ECHO_TIMEOUT, param);
     }
 
     portEXIT_CRITICAL(&mux);
 
-    *distance_left = (time - echo_start) / ROUNDTRIP;
+    param->distance = (time - echo_start) / ROUNDTRIP;
 
-    return ESP_OK;
+    // End task
+    param->error = ESP_OK;
+    vTaskDelete(NULL);
 }
 
-esp_err_t ultrasonic_measure_right_cm(const ultrasonic_sensor_t* sensor, uint32_t* distance_right)
+void ultrasonic_measure_right_cm(void* parameter)
 {
+    measure_param* param = (measure_param*)parameter;
 
+    portENTER_CRITICAL(&mux);
+
+    // Sets of ultrasonic sensor 
+    gpio_set_level(param->ultrasonic_sensor->trigger, 0);
+    esp_rom_delay_us(TRIGGER_LOW_DELAY);
+    gpio_set_level(param->ultrasonic_sensor->trigger, 1);
+    esp_rom_delay_us(TRIGGER_HIGH_DELAY);
+    gpio_set_level(param->ultrasonic_sensor->trigger, 0);
+
+    // If previous ping hasn't ended
+    if (gpio_get_level(param->ultrasonic_sensor->echo_right))
+        RETURN_CRITICAL(mux, ESP_ERR_ULTRASONIC_SENSOR_PING, param);
+
+    	
+    // Wait for echo reply from single reciever
+    uint32_t start = get_time_us();
+    while (!gpio_get_level(param->ultrasonic_sensor->echo_right))
+    {
+        if (timeout_expired(start, PING_TIMEOUT))
+            RETURN_CRITICAL(mux, ESP_ERR_ULTRASONIC_SENSOR_PING_TIMEOUT, param);
+    }
+
+    // Echo recieved continue
+    uint32_t echo_start = get_time_us();
+    uint32_t time = echo_start;
+    uint32_t meas_timeout = echo_start + MAX_DISTANCE + ROUNDTRIP;
+    while (gpio_get_level(param->ultrasonic_sensor->echo_right))
+    {
+        time = get_time_us();
+        if (timeout_expired(start, meas_timeout))
+            RETURN_CRITICAL(mux, ESP_ERR_ULTRASONIC_SENSOR_ECHO_TIMEOUT, param);
+    }
+
+    portEXIT_CRITICAL(&mux);
+
+    param->distance = (time - echo_start) / ROUNDTRIP;
+
+    // End task
+    param->error = ESP_OK;
+    vTaskDelete(NULL);
 }
 
 
@@ -97,9 +142,14 @@ esp_err_t ultrasonic_measure_cm(const ultrasonic_sensor_t* sensor, uint32_t* dis
     if (distance_left == NULL || distance_left == distance_right || sensor == NULL)
         return ESP_ERR_INVALID_ARG;
 
+    measure_param param_left, param_right;
+
     TaskHandle_t measure_left = NULL;
     TaskHandle_t measure_right = NULL;
 
-    xTaskCreate(ultrasonic_measure_left_cm, "Measure left sensor", 4096, NULL, 10, &measure_left);
-    xTaskCreatePinnedToCore(ultrasonic_measure_right_cm, "Measure lright sensor", 4096, NULL, 10, &measure_right, 1);
+    xTaskCreate(&ultrasonic_measure_left_cm, "Measure left sensor", 4096, (void*)&param_left, 10, &measure_left);
+    xTaskCreatePinnedToCore(&ultrasonic_measure_right_cm, "Measure lright sensor", 4096, (void*)&param_right, 10, &measure_right, 1);
+
+    // temp;
+    return param_left.error;
 }
